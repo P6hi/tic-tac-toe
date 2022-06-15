a31792aee54b3b85bae4f795c2dde5976fd1352e
Arthur <a-murk@mail.ee>
2022-06-15 15:55:02 +0300

    Add tile selection
    User can click on a tile and it will update the mark property of the correct object

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 const tiles = document.querySelectorAll('.tile');
 tiles.forEach((tile) => {
     tile.addEventListener('click', () => {
-        
-    })
-})
+        const tileY = parseInt(tile.dataset.y);
+        const tileX = parseInt(tile.dataset.x);
+        for (let i = 0; i < gameBoard.gameArray.length; i++) {
+            if (tileY === i) {
+                for (let j = 0; j < gameBoard.gameArray[i].length; j++)
+                if (tileX === j) {
+                    gameBoard.gameArray[i][j].mark = 'X';
+                }
+            }
+        }
+    });
+});
+
+const gameBoard = {
+gameArray: [[{mark: null, x: 1, y: 1}, {mark: null, x: 2, y: 1}, {mark: null, x: 3, y: 1}], 
+[{mark: null, x: 1, y: 2}, {mark: null, x: 2, y: 2}, {mark: null, x: 3, y: 2}], 
+[{mark: null, x: 1, y: 3}, {mark: null, x: 2, y: 3}, {mark: null, x: 3, y: 3}]]
+}
