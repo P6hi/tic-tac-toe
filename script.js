b30e308f7020be5a4c9be1100e1f370235c32235
Arthur <a-murk@mail.ee>
2022-06-16 16:02:22 +0300

    Allow user to choose a name

--- script.js
+++ script.js
@@ -1,41 +1,53 @@
 const gamePlay = (function() {
-    
     let playerMarkValue;
     let turns = 0;
-    const playerOne = player("Player 1", "X", true);
-    const playerTwo = player("Player 2", "O", false);
+    const nameBtn = document.querySelector('.name-btn');
+    const startBtn = document.querySelector('.start-btn');
+    let playerTwo = player('Player Two', 'O', false);
+    let playerOne = player('Player One', 'X', true);
+    nameBtn.addEventListener('click', () => {
+        let playerNameInput = prompt('What is your name?', '').toString();
+        console.log(typeof playerNameInput);
+        playerOne = player(playerNameInput, 'X', true);
+    });
 
-    // Marks tiles
-    const tiles = document.querySelectorAll('.tile');
-    tiles.forEach((tile) => {
-    tile.addEventListener('click', () => {
-        if (playerOne.isTurn === true) {
-            playerMarkValue = playerOne.playerMark;
-        } else {
-            playerMarkValue = playerTwo.playerMark;
-        }
-        tile.textContent = playerMarkValue;
-        const tileY = parseInt(tile.dataset.y);
-        const tileX = parseInt(tile.dataset.x);
-        for (let i = 0; i < gameBoard.gameArray.length; i++) {
-            if (tileY === i) {
-                for (let j = 0; j < gameBoard.gameArray[i].length; j++)
-                if (tileX === j) {
-                    gameBoard.gameArray[i][j].mark = playerMarkValue;
+    startBtn.addEventListener('click', () => {
+        console.log(playerOne.playerName);
+        play();
+    })
+
+    function play() {  
+        const tiles = document.querySelectorAll('.tile');
+        tiles.forEach((tile) => {
+        tile.addEventListener('click', () => {
+            if (playerOne.isTurn === true) {
+                playerMarkValue = playerOne.playerMark;
+            } else {
+                playerMarkValue = playerTwo.playerMark;
+            }
+            tile.textContent = playerMarkValue;
+            const tileY = parseInt(tile.dataset.y);
+            const tileX = parseInt(tile.dataset.x);
+            for (let i = 0; i < gameBoard.gameArray.length; i++) {
+                if (tileY === i) {
+                    for (let j = 0; j < gameBoard.gameArray[i].length; j++)
+                    if (tileX === j) {
+                        gameBoard.gameArray[i][j].mark = playerMarkValue;
+                    }
                 }
             }
-        }
-        turns += 1;
-        winCheck();
-        if (playerOne.isTurn === true) {
-            playerOne.isTurn = false;
-            playerTwo.isTurn = true;
-        } else {
-            playerOne.isTurn = true;
-            playerTwo.isTurn = false;
-        }
+            turns += 1;
+            winCheck();
+            if (playerOne.isTurn === true) {
+                playerOne.isTurn = false;
+                playerTwo.isTurn = true;
+            } else {
+                playerOne.isTurn = true;
+                playerTwo.isTurn = false;
+            }
+        });
     });
-});
+    }
     
     // Game board
     const gameBoard = {
@@ -61,7 +73,7 @@ const gamePlay = (function() {
                 if (playerOne.isTurn === true) {
                     if (gameBoard.gameArray[i][0].mark === playerOne.playerMark) {
                         if (gameBoard.gameArray[i][0 + 1].mark === playerOne.playerMark && gameBoard.gameArray[i][0 + 2].mark === playerOne.playerMark) {
-                            alert('Player One wins!');
+                            alert(playerOne.playerName + ' wins!');
                             return true;
                         } 
                     }
@@ -85,7 +97,7 @@ const gamePlay = (function() {
                 if (playerOne.isTurn === true) {
                     if (gameBoard.gameArray[0][j].mark === playerOne.playerMark) {
                         if (gameBoard.gameArray[0 + 1][j].mark === playerOne.playerMark && gameBoard.gameArray[0 + 2][j].mark === playerOne.playerMark) {
-                            alert('Player One wins!');
+                            alert(playerOne.playerName + ' wins!');
                             return true;
                         }
                     }
@@ -110,14 +122,14 @@ const gamePlay = (function() {
                 if (playerOne.isTurn === true) {
                     if (gameBoard.gameArray[0][0].mark === playerOne.playerMark) {
                         if (gameBoard.gameArray[0 + 1][0 + 1].mark === playerOne.playerMark && gameBoard.gameArray[0 + 2][0 + 2].mark === playerOne.playerMark) {
-                            alert('Player One wins!');
+                            alert(playerOne.playerName + ' wins!');
                             return true;
                         } else {
                             return false;
                         }
                     } else if (gameBoard.gameArray[0][2].mark === playerOne.playerMark) {
                         if (gameBoard.gameArray[0 + 1][2 - 1].mark === playerOne.playerMark && gameBoard.gameArray[0 + 2][2 - 2].mark === playerOne.playerMark) {
-                            alert('Player One wins!');
+                            alert(playerOne.playerName + ' wins!');
                             return true;
                         } else {
                             return false;
@@ -151,6 +163,7 @@ const gamePlay = (function() {
         let playerName = name;
         let playerMark = mark;
         return {
+            playerName,
             playerMark,
             isTurn
         }
